b043f56decfa5b4baae9dbca868e242e3f1b1f3d
Takuya Koike <takuya.koike@link-u.co.jp>
2025-05-10 22:49:00 +0900

    add: between macro

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define repe(i, n) for (int i = 0; i <= (int)(n); ++i)
 #define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
 #define defin(type, var) type var; cin >> var
+#define between(m, a, b) ((a) <= (m) && (m) <= (b))
 typedef long long ll;
 
 int main() {
